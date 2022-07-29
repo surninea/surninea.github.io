f63f32126a0302b2f3731f7298e21e5e60193782
Egor <105794152+surninea@users.noreply.github.com>
2022-07-29 10:23:05 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__navigate'),
+    menuItem = document.querySelectorAll('.header__link'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__navigate_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__navigate_active');
+        })
+    })
+})
